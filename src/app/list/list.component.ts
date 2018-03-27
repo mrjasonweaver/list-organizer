43edf990ea1b3597faf61dd6a3b3cb4ab6b6b379
mrjasonweaver <indyplanets@gmail.com>
2018-03-27 16:37:31 -0500

    truncate and show tooltip based on breakpoints

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-list',
@@ -7,16 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe([
+      Breakpoints.Web
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.activateWebLayout();
+      }
+    });
+    breakpointObserver.observe([
+      Breakpoints.Tablet
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.activateTabletLayout();
+      }
+    });
+  }
 
   public tooltipPosition;
   public id;
   public tooltipActive: boolean;
 
+  activateWebLayout() {
+    this.tooltipActive = false;
+    console.log("Web Layout activated");
+  }
+  activateTabletLayout() {
+    this.tooltipActive = true;
+    console.log("Tablet Layout activated");
+  }
+
   ngOnInit() {
     this.tooltipPosition = 'above';
     this.id = '123456789123456789123456789';
-    this.tooltipActive = true;
   }
 
 }
