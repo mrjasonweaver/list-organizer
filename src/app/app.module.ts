d94364039fd5d6ffcdce3b52bcec01f4ebbce4b5
mrjasonweaver <indyplanets@gmail.com>
2018-03-26 21:18:09 -0500

    Start list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
