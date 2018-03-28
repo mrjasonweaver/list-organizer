c476c740e0ac13e3bfc6e5208425a16923b73a2f
mrjasonweaver <indyplanets@gmail.com>
2018-03-28 17:04:34 -0500

    setting truncate values

--- src/app/pipes/truncate-pipe/truncate.pipe.spec.ts
+++ src/app/pipes/truncate-pipe/truncate.pipe.spec.ts
@@ -18,4 +18,25 @@ describe('TruncatePipe', () => {
   it('transforms "123456789" to "12345678..."', () => {
     expect(pipe.transform('123456789', 'end')).toBe('12345678...');
   });
+
+  // setting truncate value length
+  it('transforms "123456789" to "1234..."', () => {
+    expect(pipe.transform('123456789', 'end', 4)).toBe('1234...');
+  });
+
+  // setting truncate value length with odd number
+  it('transforms "123456789" to "12...89"', () => {
+    expect(pipe.transform('123456789', 'middle', 5)).toBe('12...89');
+  });
+
+  // don't truncate if value is < 9
+  it('returns orignal value', () => {
+    expect(pipe.transform('12345678')).toBe('12345678');
+  });
+
+  // don't truncate if value is equal to or less than the set truncate length value
+  it('returns orignal value when truncate length is set', () => {
+    expect(pipe.transform('1234567', 'start', 7)).toBe('1234567');
+  });
+
 });
--- src/app/pipes/truncate-pipe/truncate.pipe.ts
+++ src/app/pipes/truncate-pipe/truncate.pipe.ts
@@ -6,12 +6,16 @@ import { SlicePipe } from '@angular/common';
 })
 export class TruncatePipe extends SlicePipe implements PipeTransform {
 
-  transform(value: any, format?: any): any {
+  transform(value: any, format?: any, length?: any): any {
     let formattedValue;
-    const firstEight = super.transform(value, 0, 8);
-    const lastEight = super.transform(value, -8);
-    const firstFour = super.transform(value, 0, 4);
-    const lastFour = super.transform(value, -4);
+    const formattedLength = length ? length : 8;
+    if (value.length <= formattedLength) return value;
+    const isEven = formattedLength % 2 == 0;
+    const formattedLengthHalf = isEven ? formattedLength / 2 : (formattedLength - 1) / 2;
+    const firstEight = super.transform(value, 0, formattedLength);
+    const lastEight = super.transform(value, -formattedLength);
+    const firstFour = super.transform(value, 0, formattedLengthHalf);
+    const lastFour = super.transform(value, -formattedLengthHalf);
     const truncatedStart = `...${lastEight}`;
     const truncatedEnd = `${firstEight}...`;
     const truncatedMiddle = `${firstFour}...${lastFour}`;
