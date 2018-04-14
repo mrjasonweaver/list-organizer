c49021ed4cb3799c513a808d1201fc65d9c80a60
mrjasonweaver <indyplanets@gmail.com>
2018-04-13 22:35:52 -0500

    return new object in contact reducer

--- clientapp/src/app/reducers/contact.ts
+++ clientapp/src/app/reducers/contact.ts
@@ -5,7 +5,7 @@ import { Action } from '../actions/contacts';
 export function contactReducer(state: Contact, action: Action): Contact {
   switch (action.type) {
     case  'CONTACT_UPDATED': {
-      return action.payload;
+      return {...action.payload};
     }
     default: {
       return state;
