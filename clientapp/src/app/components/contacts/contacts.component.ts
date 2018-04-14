850cc7b7ed246582b3944c40fc5e04f835882e71
mrjasonweaver <indyplanets@gmail.com>
2018-04-14 09:07:18 -0500

    id type definition

--- clientapp/src/app/components/contacts/contacts.component.ts
+++ clientapp/src/app/components/contacts/contacts.component.ts
@@ -20,7 +20,6 @@ export class ContactsComponent implements OnInit {
   private phone$: Observable<string>;
   private status$: Observable<boolean>;
   private contactSelected: Observable<boolean>;
-  private id: string;
   private displayedColumns = ['lastName', 'firstName', 'role', 'organization', 'phone', 'action'];
 
   constructor(private store: Store<ContactsState>, private router: Router) { }
@@ -36,7 +35,7 @@ export class ContactsComponent implements OnInit {
     this.status$ = this.store.select(fromRoot.selectContactStatus);
   }
 
-  getContact(id) {
+  getContact(id: string) {
     this.router.navigate(['/contacts'], { queryParams: { selected: id } });  // change route query params so we can load selected contact with ngrx effects
   }
 
