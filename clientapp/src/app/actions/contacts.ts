97d5beb0dfde9fca8fbbdeccbeb92f701e59cec8
mrjasonweaver <indyplanets@gmail.com>
2018-04-15 18:54:13 -0500

    better type definition for the contacts reducer

--- clientapp/src/app/actions/contacts.ts
+++ clientapp/src/app/actions/contacts.ts
@@ -2,6 +2,6 @@ import { RouterAction } from '@ngrx/router-store';
 import { Contact } from '../models/contacts';
 import { State } from '../models';
 // actions
-export type ContactsUpdated = { type: 'CONTACTS_UPDATED', payload: { contacts: Contact[] }};
+export type ContactsUpdated = { type: 'CONTACTS_UPDATED', payload: Contact[] };
 export type ContactUpdated = { type: 'CONTACT_UPDATED', payload: Contact };
 export type Action = RouterAction<State> | ContactsUpdated | ContactUpdated;
--- clientapp/src/app/reducers/contacts.ts
+++ clientapp/src/app/reducers/contacts.ts
@@ -1,8 +1,8 @@
-import { Contact, ContactsState } from '../models/contacts';
+import { Contact } from '../models/contacts';
 import { Action } from '../actions/contacts';
 
 // reducer
-export function contactsReducer(state: ContactsState, action: Action): ContactsState {
+export function contactsReducer(state: Contact[], action: Action): Contact[] {
   switch (action.type) {
     case 'CONTACTS_UPDATED': {
       return action.payload;
