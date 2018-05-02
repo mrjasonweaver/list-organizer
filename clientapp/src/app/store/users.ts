08eb829c0c9dcbbe4ad3d5fd7bccadbc44df4d10
James Weaver <indyplanets@gmail.com>
2018-05-01 20:02:24 -0500

    create users observable store

---
+++ clientapp/src/app/store/users.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsersService } from '../services/users.service';
+import { IUser } from '../models/users';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+@Injectable()
+export class ContactsStore {
+
+  private users: BehaviorSubject<any> = new BehaviorSubject([]);
+
+  constructor(private usersService: UsersService) {
+    this.loadInitialData();
+  }
+
+  get users$() {
+    return this.users.asObservable();
+  }
+
+  loadInitialData() {
+    this.usersService.getUsers().subscribe(res => this.users.next(res),
+      err => console.log('Error retrieving users')
+    );
+  }
+
+}
