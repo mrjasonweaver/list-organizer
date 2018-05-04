0a3d41238cc6ba387c2f42b7a76e55b4a1df5025
mrjasonweaver <indyplanets@gmail.com>
2018-05-04 15:55:09 -0500

    issuesObject for store

--- clientapp/src/app/store/issues.ts
+++ clientapp/src/app/store/issues.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { IssuesService } from '../services/issues.service';
-import { IIssue, IParams, params } from '../models/issues';
+import { IIssuesObject, IIssue, IParams, params } from '../models/issues';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
@@ -9,26 +9,26 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 export class IssuesStore {
 
-  private _issues: BehaviorSubject<any> = new BehaviorSubject([]);
-  public readonly issues: Observable<IIssue[]> = this._issues.asObservable();
+  private _issuesObject: BehaviorSubject<any> = new BehaviorSubject({});
+  public readonly issuesObject: Observable<IIssuesObject> = this._issuesObject.asObservable();
 
   constructor(private issuesService: IssuesService) {
     this.loadInitialIssues();
   }
 
-  get issues$() {
-    return this._issues;
+  get issuesObject$() {
+    return this._issuesObject;
   }
 
   loadInitialIssues() {
     this.issuesService.getIssues(params)
-      .subscribe(res => this._issues.next(res),
+      .subscribe(res => this._issuesObject.next(res),
         err => console.log('Error retrieving issues')
       );
   }
   reloadIssues(userParams) {
     this.issuesService.getIssues(userParams)
-      .subscribe(res => this._issues.next(res),
+      .subscribe(res => this._issuesObject.next(res),
         err => console.log('Error retrieving issues')
       );
   }
