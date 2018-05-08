3ded616ecbb0a0c7a2136fca7e349cbb11d9d62f
mrjasonweaver <indyplanets@gmail.com>
2018-05-07 21:29:17 -0500

    clean up uistate model

--- clientapp/src/app/models/ui-state.ts
+++ clientapp/src/app/models/ui-state.ts
@@ -1,17 +1,9 @@
 export interface IUiState {
   actionOngoing: boolean;
   message: string;
-  perPage: string;
-  page: string;
-  sort: string;
-  order: string;
 }
 
 export const initialUiState: IUiState = {
   actionOngoing: false,
-  message: '',
-  perPage: '10',
-  page: '1',
-  sort: 'created',
-  order: 'desc'
+  message: ''
 };
